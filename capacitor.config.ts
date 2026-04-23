8a625f744a40dbd4df51c3bdf068fe624929228c
Altino-pixel <recuperarcontaff14@gmail.com>
2026-04-23 22:06:54 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.venera.auxilio',
+  appName: 'VENERA 2.0',
+  webDir: 'dist'
+};
+
+export default config;
